3b44c98b5c78b6fb72051592adeb2f45838f1765
Crystal Valdez <cvalde28@stu.southtexascollege.edu>
2024-10-04 01:13:15 -0500

    fixed rounded corners on image and card

--- src/components/MainCardSingle.vue
+++ src/components/MainCardSingle.vue
@@ -12,11 +12,11 @@ const username = faker.person.username({firstName: firstname, lastname:lastName}
 <template>
     <!-- {{ fullname }} <br /> -->
       <div class="border border-gray-400 rounded-lg shadow bg-gray-200 h-full">
-        <img :src="faker.image.urlLoremFlicker({category: 'cat'})" />
+        <img :src="faker.image.urlLoremFlicker({category: 'cat'})" class="rounded-t-xl" />
       <div class="p-4">
         <p class="mb-2 text-2xl font-bold">{{ fullname }}</p>
         <p class="mb-3 font-normal text-gray-700">{{ job_title }}</p>
         <p class="mb-3 font-normal text-gray-700">{{ bio }}</p>
     </div>
-</div>
+    </div>
 </template>
